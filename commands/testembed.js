10567ef65607d5b56c5129d53a20a2c27fffd672
jinglejinglemfer <81329095+jinglejinglemfer@users.noreply.github.com>
2021-04-09 19:16:52 -0400

    Update testembed.js

--- commands/testembed.js
+++ commands/testembed.js
@@ -6,8 +6,8 @@ exports.run = async (bot,message,args) => {
     .setTitle('*\"Hm? Whaddaya want?\"*')
     .setAuthor('KnightleyBot', 'https://i.imgur.com/xtEi0sD.png', 'https://discord.com/api/oauth2/authorize?client_id=824018254812282881&permissions=0&scope=bot')
     .setDescription('A generic Discord bot for Ace Attorney-themed servers. Includes content from AA1-AA6, AAI and AAI2.')
+    .setThumbnail('https://i.imgur.com/MdCBJP9.png')
     .addFields(
-        { name: '\u200B', value: '\u200B' },
         { name: 'generate', value: 'Names a character.', inline: true },
         { name: 'horace', value: 'Quotes Horace Knightley.', inline: true },
         { name: 'dl-6', value: 'Says the thing.', inline: true },
