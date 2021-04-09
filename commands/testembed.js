01761014bd5ce8a4332e55f7f2dea6a409302dd8
jinglejinglemfer <81329095+jinglejinglemfer@users.noreply.github.com>
2021-04-09 18:43:48 -0400

    Update testembed.js

--- commands/testembed.js
+++ commands/testembed.js
@@ -3,10 +3,11 @@ const Discord = require('discord.js');
 exports.run = async (bot,message,args) => {
     const exampleEmbed = new Discord.MessageEmbed()
     .setColor('#F8C300')
-    .setTitle('*Hm? Whaddaya want?*')
-    .setAuthor('KnightleyBot', 'https://imgur.com/a/CIgE4sC', 'https://discord.com/api/oauth2/authorize?client_id=824018254812282881&permissions=0&scope=bot')
+    .setTitle('*\"Hm? Whaddaya want?\"*')
+    .setAuthor('KnightleyBot', 'https://i.imgur.com/xtEi0sD.png', 'https://discord.com/api/oauth2/authorize?client_id=824018254812282881&permissions=0&scope=bot')
     .setDescription('A generic Discord bot for Ace Attorney-themed servers. Includes content from AA1-AA6, AAI and AAI2.')
     .addFields(
+        { name: '\u200B', value: '\u200B' },
         { name: 'generate', value: 'Names a character.', inline: true },
         { name: 'horace', value: 'Quotes Horace Knightley.', inline: true },
         { name: 'dl-6', value: 'Says the thing.', inline: true },
@@ -15,7 +16,7 @@ exports.run = async (bot,message,args) => {
         { name: 'fact', value: 'Says an Ace Attorney fact.', inline: true },
     )
     .setTimestamp()
-    .setFooter('...Horace Knightley.', 'https://imgur.com/a/CIgE4sC');
+    .setFooter('...Horace Knightley.', 'https://i.imgur.com/xtEi0sD.png');
 
     message.channel.send(exampleEmbed);
 }
